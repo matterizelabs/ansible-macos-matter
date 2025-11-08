6c988b3cb0edc52a5a31c80fd48632aad0c1286f
abu <abu@matterize.io>
2025-11-08 19:00:27 +0530

    I need to search for nixlint

--- shell.nix
+++ shell.nix
@@ -21,7 +21,7 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    export MACOS_HOST=${MACOS_HOST:-192.168.10.2
+    export MACOS_HOST=${MACOS_HOST:-192.168.10.2}
     export MACOS_SSH_USER=${MACOS_SSH_USER:-user}
 
     echo "Ansible development environment loaded"
